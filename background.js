8fdf302343c3bde78cc78733c1d8ad6aabf80867
Austin Hartzheim <inbox@austinhartzheim.me>
2016-08-22 22:46:27 +0000

    Save requests to a request log for later processing.

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
-function logRequest(requestDetails) {
-    console.log("Loading: " + requestDetails.url);
+var requestLog = [];
+
+function logRequest(request) {
+    console.log("TheFront Request: " + request.timeStamp.toString() + " " +
+                request.type + " " + request.method + " " + request.url +
+                "; Origin: " + request.originUrl);
+    requestLog.push({
+        ts: request.timeStamp,
+        type: request.type,
+        method: request.method,
+        url: request.url,  // TODO: redact to hostname
+        origin: request.originUrl  // TODO: redact to hostname
+    });
+
+    console.log("Log length is: " + requestLog.length.toString());
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
