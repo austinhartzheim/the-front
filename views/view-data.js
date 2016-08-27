081ac2c5aaae63a8bea7d0d5f241da6153851538
Austin Hartzheim <inbox@austinhartzheim.me>
2016-08-27 04:28:35 +0000

    Added the ability to download the request log.

--- views/view-data.js
+++ views/view-data.js
@@ -16,6 +16,22 @@ function loadDataFromStorage() {
     });
 }
 
+function downloadRequestLog() {
+    chrome.storage.local.get("requestLog", function(d) {
+        blob = new Blob([JSON.stringify(d, null, 2)], {type: "application/octet-stream"});
+        url = URL.createObjectURL(blob);
+
+        chrome.downloads.download({
+            url: url,
+            filename: "request-log-" + new Date().toISOString() + ".json"
+        });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function(e) {
     loadDataFromStorage();
+
+    document.getElementById("download-request-log").addEventListener("click", function(e) {
+        downloadRequestLog();
+    });
 });
