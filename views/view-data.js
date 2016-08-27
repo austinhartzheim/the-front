4cea02f79f0de879154d969f4f5867251dcc9ea6
Austin Hartzheim <inbox@austinhartzheim.me>
2016-08-27 05:50:02 +0000

    Remove old code for displaying the request log length.

--- views/view-data.js
+++ views/view-data.js
@@ -1,16 +1,4 @@
 function loadDataFromStorage() {
-    var logLength = 0;
-    // chrome.storage.local.get("logLength", (res) => {var logLength = res;});
-    chrome.storage.local.get("logLength", function(d) {
-        console.log(d.logLength);
-        console.log("Read data: " + d.logLength.toString());
-        logLength = d.logLength;
-
-        document.getElementById("logLength").value = d.logLength.toString();
-    });
-    console.log("Log length is: " + logLength.toString());
-
-    // Load request log
     chrome.storage.local.get("requestLog", function(d) {
         document.getElementById("requestLog").value = JSON.stringify(d.requestLog, null, 2);
     });
